243048ef559676cc00d73bcdb8bf13fb97a91ef8
Adam Abah <danielabah45@gmail.com>
2022-09-09 10:26:06 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 
 /**
 
- * main - Entry point
-
+* main - Entry point
  *  Description: 'the program's description'
 
  *  Return: Always 0 (Success)
@@ -11,8 +10,7 @@
  */
 
 int main(void)
-  
-{  
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0); 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0); 
 }
