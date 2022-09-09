067e9486c193e65b5ffa4f5c04d09449b1b8c929
Adam Abah <danielabah45@gmail.com>
2022-09-08 19:52:41 -0700

    lmao

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
 #include <stdio.h>
-
 /**
 * main - Entry point
 * Description: prints "\"Programming is like building a multilingual puzzle"
 * Return: 0
 */
 int main(void)
-=======
 /**
 * main - main block
 * Return: 0
 */
 int puts(void)
->>>>>>> 8fe195fc203d7b2cba8a9d9e3fad3b0a8c07b962
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
