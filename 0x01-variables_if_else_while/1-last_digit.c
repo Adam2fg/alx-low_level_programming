be9fa9762f6e075bdd82bede910e1bc95d618307
Adam Abah <danielabah45@gmail.com>
2022-09-09 18:03:03 -0700

    2022

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print alphabet in lowercase
+*Return: zero
+*/
+int main(void)
+{  
+int i = 97;
+while (i < 123)
+{
+putchar(i);      
+i++;
+}
+putchar(10);
+return (0);
+}
