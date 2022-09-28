5e12174f91eea25a17d6fefbd9401e45eb5f9afb
Adam Abah <danielabah45@gmail.com>
2022-09-28 21:44:18 +0100

    l

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string, followed by a new line.
+ * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 if (*s)
 {
+_print_rev_recursion(s + 1);
 _putchar(*s);
-_puts_recursion(s + 1);
 }
-else
-_putchar('\n');
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-recursion directory.
- */
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
