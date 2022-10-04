94dcbe174f24103db473bb36721ac55212aca810
Adam Abah <danielabah45@gmail.com>
2022-10-05 01:10:54 +0200

    super

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,7 +22,6 @@ index++;
 }
 return (len);
 }
-
 /**
  * count_words - Counts the number of words contained within a string.
  * @str: The string to be searched.
@@ -33,7 +32,7 @@ int count_words(char *str)
 {
 int index = 0, words = 0, len = 0;
 for (index = 0; *(str + index); index++)
-    len++;
+len++;
 for (index = 0; index < len; index++)
 {
 if (*(str + index) != ' ')
@@ -44,7 +43,6 @@ index += word_len(str + index);
 }
 return (words);
 }
-
 /**
  * strtow - Splits a string into words.
  * @str: The string to be split.
